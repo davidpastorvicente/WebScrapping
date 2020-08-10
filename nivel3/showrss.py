7238f1774149d0715bf10bbd0dcbede751229585
David <davidpastorvicente@gmail.com>
2020-08-10 16:35:50 +0200

    Update ShowRSS

--- nivel3/showrss.py
+++ nivel3/showrss.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
 opts = Options()
